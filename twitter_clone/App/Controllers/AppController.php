905ab91388fe1f0246f068a5a67766aa5526a559
Adlla Katarine <adllakatarine@hotmail.com>
2021-07-07 22:32:56 -0300

    Pesquisando por outros usuários.
    Pesquisando por outros usuários.

--- twitter_clone/App/Controllers/AppController.php
+++ twitter_clone/App/Controllers/AppController.php
@@ -35,5 +35,21 @@ class AppController extends Action {
             header('Location: /?login=erro');
         }
     }
+
+    public function quem_seguir() {
+        $this->validaAutenticacao();
+        $pesquisarPor = isset($_GET['pesquisarPor']) ? $_GET['pesquisarPor'] : '';
+
+        echo 'pesquisando por: '.$pesquisarPor;
+
+        $usuarios = array();
+        if(!empty($pesquisarPor)) {
+            $usuario = Container::getModel('Usuario');
+            $usuario->__set('nome', $pesquisarPor);
+            $usuarios = $usuario->getAll();
+        }
+        $this->view->usuarios = $usuarios;
+        $this->render('quemSeguir');
+    }
 }
 ?> 
